dec475426dc674d8f234349573dfce3189b126a5
rudzainy <rudzainy@gmail.com>
2016-06-05 02:43:31 +0800

    Fix logic error

--- app/controllers/api/v1/queries_controller.rb
+++ app/controllers/api/v1/queries_controller.rb
@@ -3,18 +3,19 @@ class Api::V1::QueriesController < Api::V1::ApplicationController
   # http://tinggal.herokuapp.com/api/v1/query_listings?min_price=500&max_price=1500&StateCode=SE&min_size=1000&max_size=2000
 
   def query_listings
-    listings = []
+    listings_prices = []
     Listing.all.each do |listing|
       if listing.RentalPrice.to_i >= params[:min_price].to_i && listing.RentalPrice.to_i <= params[:max_price].to_i
-        listings << listing
+        listings_prices << listing
       end
     end
-    Listing.all.each do |listing|
+    listing_sizes = []
+    listings_prices.each do |listing|
       if listing.BuildUp.to_i >= params[:min_size].to_i && listing.BuildUp.to_i <= params[:max_size].to_i
-        listings << listing
+        listing_sizes << listing
       end
     end
-    render json: { success: 'true' , listings: listings.uniq, status: 200 }
+    render json: { success: 'true' , listings: listing_sizes, status: 200 }
   end
 
   private
