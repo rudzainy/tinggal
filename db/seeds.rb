43478dae805eb710dad6f15c94143b7aa0f53734
rudzainy <rudzainy@gmail.com>
2016-06-04 22:54:08 +0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
-File.open('listings.json', 'r') do |file|
+File.open('db/listings.json', 'r') do |file|
   file.each do |line|
     listing = JSON.parse line
     Listing.create! listing
   end
 end
 
-File.open('propertyCategoryType.json', 'r') do |file|
+File.open('db/propertyCategoryType.json', 'r') do |file|
   file.each do |line|
     item = JSON.parse line
     PropertyCategoryType.create! item
   end
 end
 
-File.open('propertyGroupType.json', 'r') do |file|
+File.open('db/propertyGroupType.json', 'r') do |file|
   file.each do |line|
     item = JSON.parse line
     PropertyGroupType.create! item
   end
 end
 
-File.open('propertyType.json', 'r') do |file|
+File.open('db/propertyType.json', 'r') do |file|
   file.each do |line|
     item = JSON.parse line
     PropertyType.create! item
@@ -28,4 +28,9 @@ end
 
 User.create!([
   { username: "Rudzainy", email: "rudzainy@rudzainy.com", password: "123456", gender: "male", age_range: "20-30", maritial_status: "single", salary: "10000", user_type: "user" }
+])
+
+Agent.create!([
+  { agent_id: "12345" },
+  { agent_id: "67890" }
 ])
