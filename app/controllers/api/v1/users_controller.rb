f649f4c26b475985d42e7c95adb7995e3fd702b4
rudzainy <rudzainy@gmail.com>
2016-06-05 06:06:24 +0800

    Set user type

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,14 +8,14 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 
   def create
     if params[:reg_type] == "0"
-      user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      user = User.new(username: params[:username], email: params[:email], password: params[:password], user_type: "user")
       if user.save
         render json: { success: 'true' , message: 'Successfully created user', status: 200 }
       else
         render json: { success: 'false' , message: 'Error! ' + user.errors.full_messages.to_s, status: 422 }
       end
     elsif params[:reg_type] == "fb" or params[:reg_type] == "gl"
-      user = User.new(username: params[:username], email: params[:email], reg_type: params[:reg_type], reg_key: params[:reg_key], password: "123456")
+      user = User.new(username: params[:username], email: params[:email], reg_type: params[:reg_type], reg_key: params[:reg_key], password: "123456", user_type: "user")
       if user.save
         render json: { success: 'true' , message: 'Successfully created user', status: 200 }
       else
